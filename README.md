0ebdd2c43242b175760ab2920dd439f095bee530
anon <efimovsergeiv@gmail.com>
2024-08-26 11:37:22 +0300

    add:

---
+++ README.md
@@ -0,0 +1,15 @@
+# Run
+
+```bash
+pnpm install
+pnpm run start
+```
+
+В VSCode
+
+Устанавливаем LiveServer
+Open VSCode and type ctrl+P, type ext install ritwickdey.liveserver.
+
+GoLive внизу чтоб запустить
+
+http://127.0.0.1:5500/
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type { import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+      fontFamily: {
+        'sans': ['Open Sans', ],
+        'serif': ['Open Sans', ],
+        'mono': ['Open Sans', ],
+        'display': ['Open Sans', ],
+        'body': ['Open Sans', ],
+      },
+  },
+  plugins: [],
+}
